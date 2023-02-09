178d1d2acd40cd9c765a9957991672223dda7910
matomugo60 <matomugo60@gmail.com>
2023-02-09 13:03:00 +0300

    Complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    split = self.split(/[.!?]+/)
+    split.reject! { |word| word.strip.empty? }
+    split.count
 
   end
 end
